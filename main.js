ffe3d2eab21eb754e68714e7067c257184cb7188
Adham Alattar <73290934+adhamziad99@users.noreply.github.com>
2023-09-19 11:59:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  $('.trigger').click(function() {
+     $('.modal-wrapper').toggleClass('open');
+     return false;
+  });
+});
+$( document ).ready(function() {
+  $('.baad').click(function() {
+     $('.ore').toggleClass('open');
+     return false;
+  });
+});
+$( document ).ready(function() {
+  $('.baad1').click(function() {
+     $('.ore1').toggleClass('open');
+     return false;
+  });
+});
